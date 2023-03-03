a6311e1a895c0133c9b6f271e3d0cde35f5092d2
Lucas Paul <l.b.paul@student.tudelft.nl>
2023-03-03 20:14:16 +0100

    Changed the board interface to a class and implemented it

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -1,23 +1,105 @@
 package commons;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public interface Board {
+public class Board {
 
-    String getID(); // returns the unique board ID
+    private String id;
+    private String name;
+    private List<TaskList> taskLists;
+    private String password;
 
-    String getName(); // returns the Board name
 
-    void setName(); // sets the Board name
+    /**
+     * Constructor for when the password field is left empty
+     * @param id
+     * @param name
+     */
+    public Board(String id, String name) {
+        this.id = id;
+        this.name = name;
+        this.taskLists = new ArrayList<>();
+    }
 
-    List<TaskList> getTaskLists(); // returns a list of all TaskLists in the Board
+    /**
+     * Constructor for when the password field is filled in
+     * @param id
+     * @param name
+     * @param password
+     */
+    public Board(String id, String name, String password){
+        this.id = id;
+        this.name = name;
+        this.taskLists = new ArrayList<>();
+        this.password = password;
+    }
 
-    void addTaskList(TaskList taskList); // adds a new TaskList to the Board
+    String getID(){
+        return this.id;
+    }
 
-    // returns and removes a TaskList if it is present, does nothing and returns null otherwise
-    TaskList removeTaskList(TaskList taskList);
 
-    // String getPassword(String password); // gets the board password
+    String getName() {
+        return this.name;
+    }
 
-    // void setPassword(String password); // sets the board password
+    void setName(String name){
+        this.name = name;
+    }
+
+    List<TaskList> getTaskLists(){
+        return this.taskLists;
+    }
+
+    /**
+     * Adds a TaskList to the list of TaskLists
+     * @param taskList to be added to the list
+     */
+    void addTaskList(TaskList taskList){
+        this.taskLists.add(taskList);
+    }
+
+    /**
+     * Removes a TaskList from the list of TaskLists if it exists
+     * @param taskList to be removed
+     * @return the removed taskList if it exists and null otherwise
+     */
+    TaskList removeTaskList(TaskList taskList){
+        int index = this.taskLists.indexOf(taskList);
+        if (index == -1) return null;
+        return this.taskLists.remove(index);
+    }
+
+     String getPassword(){
+        return this.password;
+     }
+
+     void setPassword(String password){
+        this.password = password;
+     }
+
+    /**
+     * Compares two Objects to see if they are equal
+     * @param o Object to be compared
+     * @return true iff Object o is not null, of the same class and all attributes are equal
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Board board = (Board) o;
+        return Objects.equals(this.id, board.id) && Objects.equals(this.name, board.name)
+                && Objects.equals(this.taskLists, board.taskLists) && Objects.equals(this.password, board.password);
+    }
+
+    /**
+     * Generates a unique hashCode for the Object it is called on
+     * @return unique hashCode for the object
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.id, this.name, this.taskLists, this.password);
+    }
 }
