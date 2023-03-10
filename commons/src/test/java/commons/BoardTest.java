37c64319a55d56bc6c282aea4b755fc24abc100f
Lucas Paul <l.b.paul@student.tudelft.nl>
2023-03-10 18:42:11 +0100

    added 'this.' to all uses of an instance variable because of pipeline failure

--- commons/src/test/java/commons/BoardTest.java
+++ commons/src/test/java/commons/BoardTest.java
@@ -10,52 +10,52 @@ class BoardTest {
     Board b2 = new Board("b");
     Board b3 = new Board("b", "test1");
     Board b4 = new Board("bb");
-    Board b5 = b1;
+    Board b5 = this.b1;
 
     @Test
     void testId(){
-        b1.setId(1L);
-        assertEquals(1L, b1.getId());
+        this.b1.setId(1L);
+        assertEquals(1L, this.b1.getId());
     }
     @Test
     void testName() {
-        assertEquals("b", b1.getName());
-        b1.setName("b1");
-        assertEquals("b1", b1.getName());
-        b1.setName("b");
+        assertEquals("b", this.b1.getName());
+        this.b1.setName("b1");
+        assertEquals("b1", this.b1.getName());
+        this.b1.setName("b");
     }
 
     @Test
     void testPassword() {
-        assertNull(b1.getPassword());
-        assertNotNull(b3.getPassword());
-        assertEquals("test1", b3.getPassword());
-        b3.setPassword("test2");
-        assertNotNull(b3.getPassword());
-        assertEquals("test2", b3.getPassword());
+        assertNull(this.b1.getPassword());
+        assertNotNull(this.b3.getPassword());
+        assertEquals("test1", this.b3.getPassword());
+        this.b3.setPassword("test2");
+        assertNotNull(this.b3.getPassword());
+        assertEquals("test2", this.b3.getPassword());
     }
 
     @Test
     void testHashEquals() {
-        b1.setId(1L);
-        assertEquals(b1, b1);
-        assertNotEquals(b1, b2);
-        assertNotEquals(b1, b3);
-        assertNotEquals(b1, b4);
-        assertEquals(b1, b5);
+        this.b1.setId(1L);
+        assertEquals(this.b1, this.b1);
+        assertNotEquals(this.b1, this.b2);
+        assertNotEquals(this.b1, this.b3);
+        assertNotEquals(this.b1, this.b4);
+        assertEquals(this.b1, this.b5);
 
-        assertEquals(b1.hashCode(), b1.hashCode());
-        assertNotEquals(b1.hashCode(), b2.hashCode());
-        assertNotEquals(b1.hashCode(), b3.hashCode());
-        assertNotEquals(b1.hashCode(), b4.hashCode());
-        assertEquals(b1.hashCode(), b5.hashCode());
+        assertEquals(this.b1.hashCode(), this.b1.hashCode());
+        assertNotEquals(this.b1.hashCode(), this.b2.hashCode());
+        assertNotEquals(this.b1.hashCode(), this.b3.hashCode());
+        assertNotEquals(this.b1.hashCode(), this.b4.hashCode());
+        assertEquals(this.b1.hashCode(), this.b5.hashCode());
     }
 
     @Test
     void testToString(){
-        assertEquals("Board: b, 0", b1.toString());
-        assertEquals("Board: bb, 0", b4.toString());
-        b1.setId(1L);
-        assertEquals("Board: b, 1", b1.toString());
+        assertEquals("Board: b, 0", this.b1.toString());
+        assertEquals("Board: bb, 0", this.b4.toString());
+        this.b1.setId(1L);
+        assertEquals("Board: b, 1", this.b1.toString());
     }
 }
