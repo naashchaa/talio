6d1602759a8c501f579adf03878d6527d54810e6
Maxim Istomin <m.i.istomin@student.tudelft.nl>
2023-03-26 22:32:34 +0200

    removed unused imports

--- server/src/main/java/server/database/TaskRepository.java
+++ server/src/main/java/server/database/TaskRepository.java
@@ -1,13 +1,11 @@
 package server.database;
 
 import commons.Task;
-import commons.TaskList;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface TaskRepository extends JpaRepository <Task, Long> {
-    
+
     List<Task> findAllByParentTaskList_Id(long id);
 }
