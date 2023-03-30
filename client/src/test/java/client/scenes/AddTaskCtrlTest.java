375785554fc99282a11e2d685fdc627a60f29fd4
Mateo <m.a.nasse@student.tudelft.nl>
2023-03-30 16:06:09 +0200

    Created tests for AddTaskCtrl and added a getter for a test

---
+++ client/src/test/java/client/scenes/AddTaskCtrlTest.java
@@ -0,0 +1,55 @@
+package client.scenes;
+
+import client.utils.ServerUtils;
+import commons.Task;
+import commons.TaskList;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+class AddTaskCtrlTest {
+
+    AddTaskCtrl sut;
+    ServerUtils sUtils;
+    MainCtrl mainCtrl;
+
+    @BeforeEach
+    public void setup() {
+        sUtils = mock(ServerUtils.class);
+        mainCtrl = mock(MainCtrl.class);
+        sut = new AddTaskCtrl(sUtils, mainCtrl);
+    }
+
+    @Test
+    public void testMainCtrlCallsCorrectly() {
+        sut.cancelTask(null);
+        verify(mainCtrl, times(1)).showBoard();
+    }
+
+    @Test
+    public void correctlySetAndGetParentTaskListCtrl() {
+        TaskListCtrl ctrlMock = mock(TaskListCtrl.class);
+        sut.setParentTaskListCtrl(ctrlMock);
+        Assertions.assertEquals(ctrlMock, sut.getParentTaskListCtrl());
+    }
+
+    @Test
+    public void correctlyReturnsListOfTasks() {
+        TaskList mockList = mock(TaskList.class);
+        Task a = new Task("a", null, mockList, null);
+        Task b = new Task("b", null, mockList, null);
+        Task c = new Task("c", null, mockList, null);
+        List<Task> expected = new ArrayList<>();
+        expected.add(a);
+        expected.add(b);
+        expected.add(c);
+        when(sut.getTasks(any(TaskList.class))).thenReturn(expected);
+        Assertions.assertEquals(expected, sut.getTasks(mockList));
+    }
+
+}
